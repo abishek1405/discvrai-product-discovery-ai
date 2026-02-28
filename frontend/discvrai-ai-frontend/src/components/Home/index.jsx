304541a4d3d9efd7af37345b1d09382274877a33
Abishek thiru <101636164+abishek1405@users.noreply.github.com>
2026-02-28 12:24:53 +0000

    change domin name

--- frontend/discvrai-ai-frontend/src/components/Home/index.jsx
+++ frontend/discvrai-ai-frontend/src/components/Home/index.jsx
@@ -12,7 +12,7 @@ export default function Home() {
     try {
       setLoading(true);
 
-      const res = await fetch("http://localhost:5000/api/ask", {
+      const res = await fetch("https://discvrai-product-discovery-ai.onrender.com/api/ask", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
